3d864cb3b220cb27582591eb1f8d937a1690fa22
Daniel Norhøj <daniel@dnorhoj.me>
2022-08-18 21:22:57 +0200

    Small comment

--- src/schedule.py
+++ src/schedule.py
@@ -2,9 +2,13 @@ from main import main as sync_cal
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == '__main__':
+    # Create scheduler
     scheduler = BlockingScheduler()
     print("Running once before starting schedule.")
     sync_cal()
+
     print("\nStarting scheduler...")
     scheduler.add_job(sync_cal, 'cron', hour="*")
+
+    # Start blocking scheduler, which will run until program is terminated
     scheduler.start()
