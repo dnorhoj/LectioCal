7e6889752e6063833ac50293c7839381a7e3f74c
Daniel Norhøj <daniel.norhoj@gmail.com>
2022-08-19 11:18:55 +0200

    Refactored code a lot.

--- src/main.py
+++ src/main.py
@@ -1,104 +1,241 @@
 import re
 from datetime import datetime, timedelta
-from os import environ as environ
 import logging
 
 import lectio
-from tqdm import tqdm
+import icalendar
+import caldav as caldav
 
-import caldav
+class LectioCalDavSynchronizer:
+    def __init__(self, lec_inst_id, lec_username, lec_password, cal_url, cal_username, cal_password) -> None:
+        """Constructor for LectioCalDavSynchronizer
 
-log = logging.getLogger("LectioCalDAV")
+        Args:
+            lec_inst_id (_type_): Lectio institution id
+            lec_username (_type_): Lectio username
+            lec_password (_type_): Lectio password
+            cal_url (_type_): CalDav calendar URL
+            cal_username (_type_): CalDav username
+            cal_password (_type_): CalDav password
+        """
 
-def main(*, use_tqdm=False):
-    # Create lectio obj
-    lec = lectio.Lectio(environ.get("LECTIO_INST_ID"))
+        self.lec_inst_id = lec_inst_id
+        self.lec_username = lec_username
+        self.lec_password = lec_password
+        self.cal_url = cal_url
+        self.cal_username = cal_username
+        self.cal_password = cal_password
 
-    # Creds from env vars
-    lec.authenticate(
-        environ.get('LECTIO_USERNAME'),
-        environ.get('LECTIO_PASSWORD')
-    )
+        self.lec = self._lec_auth()
+        self.lec_user_id = self.lec.get_user_id()
+        self.cal = caldav.CalDavClient(
+            environ.get('CALDAV_USERNAME'),
+            environ.get('CALDAV_PASSWORD'),
+            environ.get('CALDAV_URL')
+        )
 
-    # Get calendar from CalDAV URL, username and password
-    cal = caldav.CalDavClient(
-        environ.get('CALDAV_USERNAME'),
-        environ.get('CALDAV_PASSWORD'),
-        environ.get('CALDAV_URL')
-    )
+        self.log = logging.getLogger("synchronizer")
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+    
+    def _lec_auth(self) -> lectio.Lectio:
+        """Authenticate lectio module, and return it
 
-    # Get start and end dates, without hour, minute, seconds
-    start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    end = start+timedelta(days=30)
+        Returns:
+            lectio.Lectio: Authenticated Lectio instance
+        """
+        # Create lectio obj
+        lec = lectio.Lectio(self.lec_inst_id)
 
-    # Get schedule for student
-    sched = lec.get_schedule_for_student(
-        lec.get_user_id(),
-        start,
-        end
-    )
+        # Creds from env vars
+        lec.authenticate(
+            self.lec_username,
+            self.lec_password
+        )
 
-    # List of uids, used later for deleting non-existent modules
-    uids = []
+        return lec
+    
+    @staticmethod
+    def _get_module_id(module: lectio.Module) -> str:
+        """Get lectio module id as caldav id
 
-    # Iterate over all modules
-    sched_iter = sched
-    if use_tqdm:
-        sched_iter = tqdm(sched_iter, "Importing modules into CalDAV")
-    else:
-        print("Importing modules into CalDAV")
+        Args:
+            module (lectio.Module): module
 
-    for module in sched_iter:
-        # Example: no title: 3.b Da; with title: 3.b Da - Never gonna give you up
+        Returns:
+            str: id from module url with lecmod prepended
+        """
+
+        return "lecmod"+re.search(r"absid=(.*?)&", module.url)[1]
+    
+    @staticmethod
+    def _get_module_title(module: lectio.Module) -> str:
+        """Get module title
+
+        Args:
+            module (lectio.Module): Lectio module
+
+        Returns:
+            str: module title
+        """
+
         title = module.subject
         if module.title is not None:
             title += f' - {module.title}'
         
-        # Get uid from module title get params, and append it to uids list
-        uids.append("lecmod"+re.search(r"absid=(.*?)&", module.url)[1])
-        
-        # Add module url to event description, and optionally add extra info if present
+        return title
+    
+    @staticmethod
+    def _get_module_desc(module: lectio.Module) -> str:
+        """Get module description
+
+        Args:
+            module (lectio.Module): Lectio module
+
+        Returns:
+            str: Module description, with url
+        """
+
         desc = re.match(r"(.*?)&", module.url)[1]
+        if module.room:
+            desc = module.room+"\n"+desc
         if module.extra_info:
             desc += "\n\n" + module.extra_info
         
-        # Color info (changed, deleted)
+        return desc
+    
+    @staticmethod
+    def _get_module_color(module: lectio.Module) -> str:
+        """Get module color as string
+
+        Args:
+            module (lectio.Module): Lectio module
+
+        Returns:
+            str: color as string (either None, "green" or "red")
+        """
+
         color = None
         if module.status == 1: # Module changed
             color = "green"
         elif module.status == 2: # Module deleted
             color = "red"
+        
+        return color
+    
+    def add_or_update_module(self, module: lectio.Module) -> None:
+        """Adds module to CalDav
 
-        # Save the event
-        cal.add_event(
-            uid=uids[-1],
+        Args:
+            module (lectio.Module): Lectio module
+        """
+
+        self.cal.add_event(
+            uid=self._get_module_id(module),
+            summary=self._get_module_title(module),
+            desc=self._get_module_desc(module),
+            color=self._get_module_color(module),
             start=module.start_time,
             end=module.end_time,
-            summary=title,
-            desc=desc,
-            color=color
         )
 
-    # Get all cal events within start, end
-    events = cal.get_events(start, end)
 
-    # Remove leftover events
-    print("Checking if there are any untracked modules")
-    for e in events:
-        # Extract uid from ical
-        uid = e.subcomponents[0].get("uid")
+    def event_module_equal(self, event: icalendar.Calendar, module: lectio.Module) -> bool:
+        """Compare if module and calendar is equal
 
-        if uid not in uids:
-            # Get more info for logging
-            component = e.subcomponents[0]
+        Args:
+            event (icalendar.Calendar): iCal instance
+            module (lectio.Module): Module instance
 
-            start = component.get("dtstart")
-            end = component.get("dtend")
-            summary = component.get("summary")
-            log.warning(f"Deleting module {uid}, start time: {start.dt.isoformat()}, end time: {end.dt.isoformat()}, summary: {summary}")
+        Returns:
+            bool: Whether event and module are equal
+        """
+        component = event.subcomponents[0]
 
-            cal.delete_event(uid)
+        return (component.get("uid") == self._get_module_id(module) and
+                component.get("summary") == self._get_module_title(module) and
+                component.get("description") == self._get_module_desc(module) and
+                component.get("dtstart").dt.replace(tzinfo=None) == module.start_time and
+                component.get("dtend").dt.replace(tzinfo=None) == module.end_time and
+                component.get("color") == self._get_module_color(module))
+    
+    def sync(self, start:datetime=None):
+        """Starts synchronization 30 days forward
+
+        Args:
+            start (datetime, optional): Start date. Defaults to now.
+        """
+
+        # Get start and end dates, without hour, minute, seconds
+        if start is None:
+            start = datetime.now()
+
+        start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+        end = start+timedelta(days=30)
+
+        # Get schedule for student
+        sched = self.lec.get_schedule_for_student(
+            self.lec_user_id,
+            start,
+            end
+        )
+        self.log.debug(f"Got {len(sched)} modules from lectio")
+
+        sched_lookup = [self._get_module_id(module) for module in sched]
+
+        # Get events to see which should be updated/removed/inserted
+        events = self.cal.get_events(start, end)
+        self.log.debug(f"Got {len(events)} events from CalDav")
+
+        # For printing afterwards
+        added = 0
+        removed = 0
+        updated = 0
+
+        # Remove or update modules
+        for event in events:
+            uid = event.subcomponents[0].get("uid")
+
+            try:
+                i = sched_lookup.index(uid) # Will raise exception if not found
+
+                # Module exists
+                module = sched[i]
+
+                # Remove module from schedule
+                sched_lookup.pop(i)
+                sched.pop(i)
+
+                # Only update if changed
+                if not self.event_module_equal(event, module):
+                    updated += 1
+                    self.add_or_update_module(module)
+
+            except ValueError:
+                # Module does not exist in lectio's schedule
+                # Therefore we delete it from caldav
+                removed += 1
+                self.cal.delete_event(uid)
+        
+        # Add remaining modules (new modules)
+        for module in sched:
+            added += 1
+            self.add_or_update_module(module)
+        
+        self.log.info(f"Updated {updated} events\nAdded {added} events\nRemoved {removed} events")
 
 if __name__ == '__main__':
+    from os import environ
+
+    # Run on main
+
     print("Starting Lectio.py ft. CalDAV")
-    main(use_tqdm=True)
+    sync = LectioCalDavSynchronizer(
+        lec_inst_id=environ["LECTIO_INST_ID"],
+        lec_username=environ["LECTIO_USERNAME"],
+        lec_password=environ["LECTIO_PASSWORD"],
+        cal_url=environ["CALDAV_URL"],
+        cal_username=environ["CALDAV_USERNAME"],
+        cal_password=environ["CALDAV_PASSWORD"],
+    )
+
+    sync.sync()
--- src/schedule.py
+++ src/schedule.py
@@ -1,14 +1,23 @@
-from main import main as sync_cal
+from main import LectioCalDavSynchronizer
 from apscheduler.schedulers.blocking import BlockingScheduler
+from os import environ
 
-if __name__ == '__main__':
-    # Create scheduler
-    scheduler = BlockingScheduler()
-    print("Running once before starting schedule.")
-    sync_cal()
+# Create scheduler
+scheduler = BlockingScheduler()
+synchronizer = LectioCalDavSynchronizer(
+    lec_inst_id=environ["LECTIO_INST_ID"],
+    lec_username=environ["LECTIO_USERNAME"],
+    lec_password=environ["LECTIO_PASSWORD"],
+    cal_url=environ["CALDAV_URL"],
+    cal_username=environ["CALDAV_USERNAME"],
+    cal_password=environ["CALDAV_PASSWORD"],
+)
 
-    print("\nStarting scheduler...")
-    scheduler.add_job(sync_cal, 'cron', hour="*")
+print("Running once before starting schedule.")
+synchronizer.sync()
 
-    # Start blocking scheduler, which will run until program is terminated
-    scheduler.start()
+print("\nStarting scheduler...")
+scheduler.add_job(synchronizer.sync, 'cron', hour="*")
+
+# Start blocking scheduler, which will run until program is terminated
+scheduler.start()
